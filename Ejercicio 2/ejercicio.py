02726006cee249fbc0723ac48ffe419c32a6d390
Arabath <axelhobrecht@hotmail.com>
2021-09-10 20:57:16 -0300

    Inicio del ejercicio 2

---
+++ Ejercicio 2/ejercicio.py
@@ -0,0 +1,14 @@
+""" => Ejercicio 2:
+    Realiza una función que tome como parametro una frase y devuelva una 
+    lista de palabras en mayuscula. 
+
+      Ej: 
+        mi_frase = "Este sabado tenemos el ultimo encuentro online"
+
+        palabras = convertir(mi_frase)
+
+        print(palabras)
+
+        >>> ["ESTE", "SABADO", "TENEMOS", "EL", "ULTIMO", "ENCUENTRO", "ONLINE"] """
+
+    
