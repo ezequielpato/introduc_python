fe4a039b324900040a8156a867cb2f3bfc9f4e54
Arabath <axelhobrecht@hotmail.com>
2021-09-10 22:14:31 -0300

    ejercicio 2 corregido

--- Ejercicio 2/ejercicio.py
+++ Ejercicio 2/ejercicio.py
@@ -11,4 +11,34 @@
 
         >>> ["ESTE", "SABADO", "TENEMOS", "EL", "ULTIMO", "ENCUENTRO", "ONLINE"] """
 
+
+
+# mi_frase = "Este sabado tenemos el ultimo encuentro online"
+
+# def convertir(lista: list[str]):
+#     frase_split 
+
+# res = list(filter(lambda x: x % 2 == 0 and x != 0, [0,1,2,3,4]))
+# print(res)
+
+# res2 = list(filter(lambda x: x % 2 != 0 and x != 0, [0,1,2,3,4]))
+# print(res2)
+
+
+def convertir(frase: str):
+    """ La función recibe como parámetro una cadena de caracteres y 
+        devuelve una lista de elementos en mayusculas """
+    frase_split = frase.split()
+
+    # frase_mayus = list(map(lambda mayus:  mayus.upper(), frase_split))
     
+    # return frase_mayus
+
+    return list(map(str.upper, frase_split))
+
+
+mi_frase = "Este sabado tenemos el ultimo encuentro online"
+palabras = convertir(mi_frase)
+
+print(palabras)
+
