a0579d64c5de012d4def62df47221455c79ea35b
pmNiko <nikolas090189@gmail.com>
2021-09-10 22:28:07 -0300

    Ejercicio 3 ok

--- Ejercicio 3/ejercicio.py
+++ Ejercicio 3/ejercicio.py
@@ -1,1 +1,29 @@
-print("Naranja")
+from functools import reduce
+from operator import add
+"""   => Ejercicio 3:
+    Realiza una fn acumulado, que devuelva el valor acumulado 
+    de la suma de una lista de numeros mas 100.
+
+    Ej: 
+      mis_numeros = [2, 4, 6]
+
+      valor_final = acumulado(mis_numeros)
+
+      print(mis_numeros)
+
+      >>> 112 """
+
+# lista de numeros
+mis_numeros = [2, 4, 6]
+
+
+def acumulado(una_lista: list[int], inicio_acumulador=0) -> int:
+    """ La fn recibe como param una lista de numeros,
+      devuelve el valor acumulado inicializando el acumulador 
+      en un valor determinado."""
+    return reduce(add, una_lista, inicio_acumulador)
+
+
+valor_final = acumulado(mis_numeros, 100)
+
+print(valor_final)
